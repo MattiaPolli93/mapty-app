98cffee8f53fe0e9890c2ef87ed9924eace1b70c
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-11-24 17:05:04 +0100

    Enable use of map markers

--- js/script.js
+++ js/script.js
@@ -12,9 +12,8 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 if (navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position) {
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
     const coords = [latitude, longitude];
 
@@ -23,11 +22,18 @@ navigator.geolocation.getCurrentPosition(function(position) {
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
     }).addTo(map);
+    
+    map.on("click", function(mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
 
-    L.marker(coords).addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
-
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup"
+        })).setPopupContent("Workout").openPopup();
+    });
 }, function() {
     alert("Could not get position!");
 });
